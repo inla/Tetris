b69134520b3c444784418999abd4a88ae3a6c859
Inka L <inka.lappalainen@hotmail.com>
2016-10-20 23:49:40 +0300

    raportit

--- tetris/src/test/java/tetris/logic/BoardTest.java
+++ tetris/src/test/java/tetris/logic/BoardTest.java
@@ -100,6 +100,12 @@ public class BoardTest {
         b.getBoard()[0][1] = 0;
         assertFalse(b.isRowFull(0));
     }
+    
+    @Test
+    public void testOnkoRiviTaysiPalauttaaFalseLaudanUlkopuolelta() {
+        assertFalse(b.isRowFull(22));
+        assertFalse(b.isRowFull(-1));
+    }
 
     @Test
     public void testGetPointPalauttaaNollaLaudanUlkoOikealta() {
